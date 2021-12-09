05fc814f6f77175656b4909d5842ac29bd0d0017
lweyand <nest_of_muzo@hotmail.com>
2021-12-09 17:17:42 +0100

    feat(step_6): mock return from gcloud command line

--- tests/test_install_trainee_environment.sh
+++ tests/test_install_trainee_environment.sh
@@ -15,6 +15,20 @@ SCRIPT_PATH="${__root_tst}/install_trainee_environment.sh"
 # this function has behavior depending on the command line.
 gcloud() {
   echo "${FUNCNAME[0]} $*" >> gcloud_log
+
+  case "$*" in
+    'organizations list --filter=DISPLAY_NAME=zenika.com --format=value(ID)')
+      printf "10000000000"
+      return 0
+      ;;
+    'alpha billing accounts list --filter=NAME:Facturation Zenika --format=value(ACCOUNT_ID)')
+      printf "AAAAAA-BBBBBB-CCCCCC"
+      return 0
+      ;;
+    *)
+      return 0
+      ;;
+  esac
 }
 
 tearDown() {
@@ -43,6 +57,27 @@ test_get_cluster_credentials_should_be_successful() {
   assertEquals "Wrong return code" '0' "${code}"
   assertEquals 'Wrong gcloud cmd' 'gcloud container clusters get-credentials formation-ci --region europe-west1 --project formation-ci-static_user' "$(cat gcloud_log)"
   assertEquals 'Wrong result' '## Get credentials other_user' "${result}"
+}
+
+test_create_cluster_should_be_successful() {
+  USER=static_user source "${SCRIPT_PATH}" --source-only
+  # workaround: disable 'set -o errexit'
+  set +e
+  result=$(create_cluster)
+  code=$?
+
+  assertEquals "Wrong return code" '0' "${code}"
+  assertEquals 'Wrong result' '## Create project formation-ci-static_user
+## Associate project formation-ci-static_user and billing account
+## Enable container.googleapis.com API
+## Create cluster formation-ci' "${result}"
+  assertEquals 'gcloud config unset project
+gcloud organizations list --filter=DISPLAY_NAME=zenika.com --format=value(ID)
+gcloud projects create formation-ci-static_user --organization=10000000000
+gcloud alpha billing accounts list --filter=NAME:Facturation Zenika --format=value(ACCOUNT_ID)
+gcloud alpha billing projects link formation-ci-static_user --billing-account AAAAAA-BBBBBB-CCCCCC
+gcloud services enable container.googleapis.com --project formation-ci-static_user
+gcloud container clusters create formation-ci --region europe-west1 --project formation-ci-static_user --preemptible --machine-type e2-standard-8 --num-nodes 1 --min-nodes 0 --max-nodes 3 --enable-autorepair --enable-autoscaling' "$(cat gcloud_log)"
 }
 
 # Eat all command-line arguments before calling shunit2.
